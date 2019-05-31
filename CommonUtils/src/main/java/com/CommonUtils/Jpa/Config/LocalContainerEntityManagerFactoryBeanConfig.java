bf8e11681166ca243fae9ed6f8bb9a470cc4395f
AntoniChan <364770752@qq.com>
2019-05-31 17:04:19 +0800

    JPA配置优化

--- CommonUtils/src/main/java/com/CommonUtils/Jpa/Config/LocalContainerEntityManagerFactoryBeanConfig.java
+++ CommonUtils/src/main/java/com/CommonUtils/Jpa/Config/LocalContainerEntityManagerFactoryBeanConfig.java
@@ -9,23 +9,30 @@ import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.persistenceunit.PersistenceUnitManager;
 import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import com.CommonUtils.Utils.CollectionUtils.CustomCollections.HashMap;
 
+/**
+ * JpaProperties请用@Resource引用
+ * PersistenceUnitManager请用@Autowired(required = false)引用
+ * 
+ * */
 public final class LocalContainerEntityManagerFactoryBeanConfig 
 {
 	private LocalContainerEntityManagerFactoryBeanConfig() {}
 	
-	public static LocalContainerEntityManagerFactoryBean getInstance(final EntityManagerFactoryBuilder builder,
-																	 final DataSource dataSource,
-																	 final JpaProperties jpaProperties,
-																	 final Database database,
+	public static LocalContainerEntityManagerFactoryBean getInstance(final JpaProperties jpaProperties,
 																	 final String packagesToScan,
-																	 final String persistenceUnit)
+																	 final String persistenceUnit,
+																	 final EntityManagerFactoryBuilder builder,
+																	 final DataSource dataSource)
 	{
+		
 		return builder.dataSource(dataSource)
-					  .properties(getInstance(dataSource, jpaProperties, database))
+					  //.properties(getVendorProperties(dataSource, jpaProperties, database))
 					  .packages(packagesToScan)
 					  .persistenceUnit(persistenceUnit)
 					  .build();
@@ -34,15 +41,29 @@ public final class LocalContainerEntityManagerFactoryBeanConfig
 	public static JpaTransactionManager getJpaTransactionManager(final LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean)
 	{ return new JpaTransactionManager(localContainerEntityManagerFactoryBean.getObject()); }
 	
-	public EntityManager getEntityManager(final LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean) 
+	public static EntityManager getEntityManager(final LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean) 
 	{ return localContainerEntityManagerFactoryBean.getObject().createEntityManager(); }
 	
-	private static Map<String, String> getInstance(final DataSource dataSource,
-												   final JpaProperties jpaProperties,
-												   final Database database)
+	public static EntityManagerFactoryBuilder getEntityManagerFactoryBuilder(final PersistenceUnitManager persistenceUnitManager, 
+																			 final JpaProperties jpaProperties,
+																			 final Database database,
+																			 final DataSource dataSource)
+	{
+		return new EntityManagerFactoryBuilder
+			   (
+					   new HibernateJpaVendorAdapter(), 
+					   getVendorProperties(dataSource, jpaProperties, database), 
+					   persistenceUnitManager
+			   );
+	}
+	
+	private static Map<String, String> getVendorProperties(final DataSource dataSource,
+												           final JpaProperties jpaProperties,
+												           final Database database)
 	{
 		Map<String, String> properties = new HashMap<String, String>()
-				.put("hibernate.hbm2ddl.auto", "update").getMap();
+				.put("hibernate.hbm2ddl.auto", "update")
+				.getMap();
 
 		jpaProperties.setShowSql(true);
 		jpaProperties.setProperties(properties);
