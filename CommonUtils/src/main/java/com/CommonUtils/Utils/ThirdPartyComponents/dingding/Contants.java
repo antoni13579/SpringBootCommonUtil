d65cc865a5f385b6d9a25da439321fb9f3a553ff
antoni13579 <364770752@qq.com>
2019-07-25 13:48:51 +0800

    加入钉钉操作工具类

---
+++ CommonUtils/src/main/java/com/CommonUtils/Utils/ThirdPartyComponents/dingding/Contants.java
@@ -0,0 +1,20 @@
+package com.CommonUtils.Utils.ThirdPartyComponents.dingding;
+
+public final class Contants 
+{
+	private Contants() {}
+	
+	public static final String GET_TOKEN_URL = "https://oapi.dingtalk.com/gettoken";
+	
+	public static final String GET_SSO_TOKEN_URL = "https://oapi.dingtalk.com/sso/gettoken";
+	
+	public static final String GET_USER_INFO_URL = "https://oapi.dingtalk.com/user/getuserinfo";
+	
+	public static final String GET_SSO_USER_INFO_URL = "https://oapi.dingtalk.com/sso/getuserinfo";
+	
+	public static final String GET_USER_DETAIL_URL = "https://oapi.dingtalk.com/user/get";
+	
+	public static final String GET_DEPT_MEMBER_URL = "https://oapi.dingtalk.com/user/getDeptMember";
+	
+	public static final String GET_DEPT_MEMBER_SIMPLE_LIST_URL = "https://oapi.dingtalk.com/user/simplelist";
+}
