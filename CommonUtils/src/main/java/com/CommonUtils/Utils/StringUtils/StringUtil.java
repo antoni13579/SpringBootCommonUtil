8fe0c579199de0e89817894f27f27cc53f52635d
antoni13579 <364770752@qq.com>
2019-08-12 10:24:59 +0800

    分词工具迁移到字符串工具那里

--- CommonUtils/src/main/java/com/CommonUtils/Utils/StringUtils/StringUtil.java
+++ CommonUtils/src/main/java/com/CommonUtils/Utils/StringUtils/StringUtil.java
@@ -6,17 +6,24 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.wltea.analyzer.core.IKSegmenter;
 import org.wltea.analyzer.core.Lexeme;
 
+import com.CommonUtils.Utils.ArrayUtils.ArrayUtil;
+import com.CommonUtils.Utils.CollectionUtils.JavaCollectionsUtil;
 import com.CommonUtils.Utils.CommonUtils.CommonUtil;
 import com.CommonUtils.Utils.DateUtils.DateContants;
 import com.CommonUtils.Utils.DateUtils.DateFormat;
 import com.CommonUtils.Utils.DateUtils.DateUtil;
+import com.CommonUtils.Utils.StringUtils.Bean.Calculation;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -293,4 +300,70 @@ public final class StringUtil
 	 */
 	public static boolean isNum(final String val) 
 	{ return isStrEmpty(val) ? false : val.matches(StringContants.PATTERN_7); }
+	
+	public static Optional<Calculation> statistics(final String text1, final String text2)
+	{
+		//计算类
+        Calculation calculation = null;
+        
+		if (StringUtil.isStrEmpty(text1) || StringUtil.isStrEmpty(text2))
+		{ return Optional.ofNullable(calculation); }
+		
+		Map<String,int[]> resultMap = new HashMap<>();
+		
+		//统计
+        statistics(resultMap, StringUtil.divideText(text1),1);
+        statistics(resultMap, StringUtil.divideText(text2),0);
+        
+        calculation = new Calculation();
+        Iterator<Map.Entry<String, int[]>> entries = resultMap.entrySet().iterator();
+		while (entries.hasNext())
+		{
+			Map.Entry<String, int[]> entry = entries.next();
+			int[] value = entry.getValue();
+			
+			BigDecimal numerator = calculation.getNumerator();
+			BigDecimal elementA = calculation.getElementA();
+			BigDecimal elementB = calculation.getElementB();
+			
+			calculation.setNumerator(numerator.add(new BigDecimal(value[0] * value[1])));
+            calculation.setElementA(elementA.add(new BigDecimal(value[0] * value[0])));
+            calculation.setElementB(elementB.add(new BigDecimal(value[1] * value[1])));
+		}
+        
+        return Optional.ofNullable(calculation); 
+	}
+	
+	 /**
+     * 组合词频向量
+     
+     * @param words
+     * @param direction
+     * @return
+     */
+    private static void statistics(Map<String,int[]> map,List<String> words ,int direction)
+    {
+        if(JavaCollectionsUtil.isCollectionEmpty(words))
+        { return; }
+        
+        int[] in = null;
+        boolean flag = direction == 1 ? true : false;     //判断不同句子
+        for (String word : words)
+        {
+            int[] wordD = map.get(word);
+            
+            if(ArrayUtil.isArrayEmpty(wordD))
+            {
+                if(flag) { in = new int[]{1, 0}; }
+                else { in = new int[]{0, 1}; }
+                map.put(word,in);
+            }
+            
+            else
+            {
+                if(flag) { wordD[0]++; }
+                else { wordD[1]++; }
+            }
+        }
+    }
 }
