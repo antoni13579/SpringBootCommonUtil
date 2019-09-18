2da46496ed239d51ef9136478f5bd45340c37584
antoni13579 <364770752@qq.com>
2019-09-18 14:05:49 +0800

    修改一下事务拦截的日志信息

--- CommonUtils/src/main/java/com/CommonUtils/Utils/Aops/Aspects/TransactionalJtaAnnotationAspect.java
+++ CommonUtils/src/main/java/com/CommonUtils/Utils/Aops/Aspects/TransactionalJtaAnnotationAspect.java
@@ -26,7 +26,7 @@ public class TransactionalJtaAnnotationAspect
 		try
 		{
 			JtaTransactionManager jtaTransactionManager = BeanUtilServiceImpl.getBean(transactionManagerName, JtaTransactionManager.class)
-																			 .orElseThrow(() -> { return new Exception("找不到指定的事务管理器！！需查找的事务管理器名字为：" + transactionManagerName); });
+																			 .orElseThrow(() -> { return new Exception("找不到指定的分布式事务管理器！！需查找的分布式事务管理器名字为：" + transactionManagerName); });
 			jtaTransactionManager.getUserTransaction().begin();
 			result = pjp.proceed();
 			jtaTransactionManager.getUserTransaction().commit();
